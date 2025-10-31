ef922247b3d3bc0e9b0c1a1dca88c4d4aff8d555
strozz1 <oscar.gonzalez.strozzi14@gmail.com>
2025-10-31 15:35:01 +0100

    player animations

---
+++ Assets/Scripts/WaterScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class WaterScript : MonoBehaviour
+{
+
+[Range(2,500)]
+    public int NumOfVertices = 70;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
