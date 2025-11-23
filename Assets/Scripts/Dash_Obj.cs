e83df6607a0b70f91b35b9bd609c64773a9521d6
luchs3185 <antoniorg024@gmail.com>
2025-11-23 12:41:58 +0100

    lvl1 coorecciones

---
+++ Assets/Scripts/Dash_Obj.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Dash_Obj : MonoBehaviour
+{
+    [Header("Configuración del pickup")]
+    /*public GameObject pickupEffect; // opcional, un efecto visual
+    public AudioClip pickupSound;*/   // opcional, sonido
+    public float destroyDelay = 0.1f;
+
+    private bool collected = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Comprobamos si quien entra es el jugador
+        Player player = other.GetComponent<Player>();
+
+        if (player != null && !collected)
+        {
+            collected = true;
+
+            // Aumentamos su número máximo de saltos
+            player.dashobj = true;
+
+            //Efectos opcionales
+           /* if (pickupEffect != null)
+                Instantiate(pickupEffect, transform.position, Quaternion.identity);
+
+            if (pickupSound != null)
+                AudioSource.PlayClipAtPoint(pickupSound, transform.position);*/
+
+            //Destruimos el objeto
+            Destroy(gameObject, destroyDelay);
+        }
+    }
+}
