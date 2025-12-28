2ff2587d98bed0744d3219a71d2cbf1ac4f9dc45
silvs04 <silviamayorgaferrer@gmail.com>
2025-12-28 16:03:30 +0100

    Menu de pausa hecho
    - nuveo diseño
    - boton de mapa de controles
    - Mapa de controles
    - añadido algunas plataformas pero hay que cambiar como se añaden

--- Assets/Scripts/MenuPausa.cs
+++ Assets/Scripts/MenuPausa.cs
@@ -8,6 +8,9 @@ public class MenuPausa : MonoBehaviour
     [SerializeField] private GameObject botonPausa;
     [SerializeField] private GameObject menuPausa;
 
+    [Header("UI Controles")]
+    [SerializeField] private GameObject menuControles;
+
     public void Pausar()
     {
         Time.timeScale = 0f;
@@ -31,5 +34,18 @@ public class MenuPausa : MonoBehaviour
     public void Cerrar()
     {
         Application.Quit(); 
+    }
+   
+    //para mapa de controles
+    public void AbrirControles()
+    {
+        menuPausa.SetActive(false); //el menu de pausa desaparece
+        menuControles.SetActive(true); //el menu de controles aparece
+    }
+
+    public void CerrarControles()
+    {
+        menuControles.SetActive(false); //el menu de controles desaparece
+        menuPausa.SetActive(true); //el menu de pausa aparece
     }
 }
