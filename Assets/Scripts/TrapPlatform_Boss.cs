a559bbdef8fc81a6156a91d54f80758b7f3906f1
luchs3185 <antoniorg024@gmail.com>
2026-01-09 14:46:53 +0100

    Waterfall tilemap, various bugs fixed

---
+++ Assets/Scripts/TrapPlatform_Boss.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class TrapPlatform_Boss : MonoBehaviour
+{
+    [Header("Tiempo antes de desaparecer")]
+    public float disappearDelay = 0.8f; // segundos antes de desaparecer
+    public GameObject objectToActivateOnDeath;
+    private bool triggered = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Si choca con el jugador
+        if (!triggered && collision.gameObject.CompareTag("Player"))
+        {
+            triggered = true;
+            Invoke(nameof(Disappear), disappearDelay);
+        }
+    }
+
+    private void Disappear()
+    {
+        if (objectToActivateOnDeath != null)
+        {
+            objectToActivateOnDeath.SetActive(true);
+        }
+        gameObject.SetActive(false); // desaparece
+    }
+}
